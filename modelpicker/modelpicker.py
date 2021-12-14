19c108e191649fd1b6d6370af1a16f547662038d
Nezihe Merve Gürel <nezihemervegurel@gmail.com>
2021-12-14 21:05:21 +0100

    Update modelpicker.py

--- modelpicker/modelpicker.py
+++ modelpicker/modelpicker.py
@@ -15,6 +15,7 @@ def model_picker(predictions, labelset, budget):
     # Initializations of hyperparameters
     eta_t = np.sqrt(np.log(num_models)/2) # initialize the hyperparameter
     bias_scale = 1 # this linear scaling parameter is added to boost the coin tossing bias in case needed
+    cost = 0 # keep record of how many instances are queried
 
     # Shuffle the indices to reduce time-dependency
     shuffled_indices = np.random.permutation(num_instances)
@@ -25,30 +26,36 @@ def model_picker(predictions, labelset, budget):
     loss_t = np.zeros(num_models) # loss per models
     posterior_t = np.ones(num_models)/num_models
 
-    # For each streaming data instance
-    for t in np.arange(1, num_instances+1, 1):
 
-        # Edit eta
-        eta_t = eta_t / np.sqrt(t)
+    while cost <= budget: # repeat while budget is not exceeded
+        # For each streaming data instance
+        for t in np.arange(1, num_instances+1, 1):
+
+            # Edit eta
+            eta_t = eta_t / np.sqrt(t)
 
 
-        posterior_t = np.exp(-eta_t * (loss_t - np.min(loss_t)))
-        # Note that above equation is equivalent to np.exp(-eta * loss_t).
-        # `-np.min(loss_t)` is applied only to avoid entries being near zero for large eta*loss_t values before the normalization
-        posterior_t  /= np.sum(posterior_t)  # normalize
+            posterior_t = np.exp(-eta_t * (loss_t - np.min(loss_t)))
+            # Note that above equation is equivalent to np.exp(-eta * loss_t).
+            # `-np.min(loss_t)` is applied only to avoid entries being near zero for large eta*loss_t values before the normalization
+            posterior_t  /= np.sum(posterior_t)  # normalize
 
-        ### Toss a coin if xt is in the region of disagreement, else skip
-        if len(np.unique(predictions[t - 1, :])) == 1:
-            zt = 0
-        else:
-            (zt, ut) = _coin_tossing(predictions, posterior_t, bias_scale)
+            ### Toss a coin if xt is in the region of disagreement, else skip
+            if len(np.unique(predictions[t - 1, :])) == 1:
+                zt = 0
+            else:
+                (zt, ut) = _coin_tossing(predictions, posterior_t, labelset, bias_scale)
 
-        # If the coin is HEADS, query the label and update the posterior. Else no update necessary
-        if zt == 1:
-            label_t = input(...)
-            loss_t += (np.array((predictions[t-1, :] != label_t]) * 1) / ut)
-            loss_t = loss_t.reshape(num_models, 1)
-            loss_t = np.squeeze(np.asarray(loss_t))
+            # Update the cost
+            cost += zt
+
+            # If the coin is HEADS, query the label and update the posterior. Else no update necessary
+            if zt == 1:
+                print("Please enter the label for instance with ID "+str(shuffled_indices[t-1])+":")
+                label_t = input()
+                loss_t += (np.array((predictions[t-1, :] != label_t) * 1) / ut)
+                loss_t = loss_t.reshape(num_models, 1)
+                loss_t = np.squeeze(np.asarray(loss_t))
 
     bestmodel = np.argmax(posterior_t)
 
@@ -57,15 +64,16 @@ def model_picker(predictions, labelset, budget):
 
 ###
 
-def _coin_tossing(pred, post, num_classes, scale):
+def _coin_tossing(pred, post, labelset, scale):
 
     ### Compute ut
 
     # Initialize possible u_t's
+    num_classes = len(labelset)
     ut_list = np.zeros(num_classes)
 
     # Repeat for each class
-    for c in range(num_classes):
+    for c in labelset:
         # Compute the loss of models if the label of the streamed data is "c"
         loss_c = np.array(pred != c)*1
         #
